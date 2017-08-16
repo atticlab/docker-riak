d16968589cc721a36147444cb5cf84a68dc52a54
AtticLab <humaniq@static.17.51.201.138.clients.your-server.de>
2017-08-16 15:31:38 +0200

    fixed prerequisites.sh

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -2,9 +2,9 @@
 
 # Install docker
 apt update
-+apt -y install git curl make apt-transport-https ca-certificates gnupg2 software-properties-common
-+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt -y install git curl make apt-transport-https ca-certificates gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce
 service docker start
